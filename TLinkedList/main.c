b9a98af866cc0dc177e06d62e8155d0088a089b5
Rafael Lima <rafaellima@MacBook-Pro-de-Rafael.local>
2025-04-23 11:09:33 -0300

    Exercícios resolvidos

--- TLinkedList/main.c
+++ TLinkedList/main.c
@@ -10,14 +10,18 @@ int main(){
     TLinkedList* lista1 = list_create();
     TLinkedList* lista2 = list_create();
     for(i=0; i<3; i++){
-        list_insert_begin(lista1, V[i]);
-        list_insert_end(lista2, V[i]);
+        if(!list_insert_begin(lista1, V[i]))
+            printf("Nao consegui inserir na lista1 o valor V[%d] = %d\n", i, V[i]) ;   
+        if(!list_insert_end(lista2, V[i]))
+            printf("Nao consegui inserir na lista2 o valor V[%d] = %d\n", i, V[i]) ;   
     }
     TLinkedList* lista3 = list_concatenate(lista1, lista2);
     list_print(lista3);
-    puts("Deletando 4 primeiros elementos");
+    unsigned int n=4;
+    printf("Deletando %u primeiros elementos\n", n);
     //list_delete_begin(lista3);    
-    list_delete_n(lista3, 4);
+    if(!list_delete_n(lista3, n))
+        printf("Nao foi possível excluir %u elementos!\n", n);
     list_print(lista3);
     return 0;
 }
