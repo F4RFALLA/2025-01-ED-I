972aec30f549ee564ba47aba9bf88b5aaac8c14b
Rafael Lima <rafaellima@MacBook-Pro-de-Rafael.local>
2025-05-14 11:10:40 -0300

    Lista circular inicio, end, print

--- TCircList/TCircList.c
+++ TCircList/TCircList.c
@@ -20,26 +20,73 @@ TCircList* list_create(){
     return nova;
 }
 
-bool list_insert_begin(TCircList* lista, int info){
-    //TODO
-    return false;
+bool list_insert_end(TCircList* lista, int info){
+    TNo* novo = TNo_createNFill(info);
+    if(novo == NULL)
+        return false;
+
+    if(lista->inicio==NULL){
+        lista->inicio = novo;
+        novo->prox = novo; 
+        //Forma equivalente
+        //lista->inicio->prox = lista->inicio; 
+    }else{
+        //3) Percorrer a lista, e achar o último elemento
+        TNo* aux = lista->inicio;
+        //Quem é o ultimo nó?
+        //aux->prox == lista->inicio;
+        while(aux->prox != lista->inicio)
+            aux = aux->prox;
+        // aux sera nosso ultimo elemento
+        //4) Último elemento passa a apontar para o novo nó;
+        aux->prox = novo;
+        //5) Próximo de novo deve apontar para o início;
+        novo->prox = lista->inicio;
+        //6) Início passa a apontar para o novo nó;
+        //lista->inicio = novo;
+    }
+    return true;
 }
 
-bool list_insert_end(TCircList* lista, int info){
-    //TODO
-    return false;
+bool list_insert_begin(TCircList* lista, int info){
+    TNo* novo = TNo_createNFill(info);
+    if(novo == NULL)
+        return false;
+    if(lista->inicio!=NULL){
+        //3) Percorrer a lista, e achar o último elemento
+        TNo* aux = lista->inicio;
+        //Quem é o ultimo nó?
+        //aux->prox == lista->inicio;
+        while(aux->prox != lista->inicio)
+            aux = aux->prox;
+        // aux sera nosso ultimo elemento
+        //4) Último elemento passa a apontar para o novo nó;
+        aux->prox = novo;
+        //5) Próximo de novo deve apontar para o início;
+        novo->prox = lista->inicio;
+        //6) Início passa a apontar para o novo nó; 
+    }
+    lista->inicio = novo;
+    return true;
 }
 
 
 void list_print(TCircList* lista){
-    //TODO
+    TNo* aux = lista->inicio;
+    do{
+        if(aux!=NULL){        
+            printf("%d, ", aux->info);
+            aux = aux->prox;
+        }
+    }while(aux != lista->inicio);    
+    putchar('\n');
 }
 
 TNo* TNo_createNFill(int info){
     TNo* novo = malloc(sizeof(TNo));
     if(novo){
         novo->info = info;
-        novo->prox = NULL;
+        novo->prox = novo;
     }
     return novo;
 }
--- TCircList/main.c
+++ TCircList/main.c
@@ -8,20 +8,11 @@
 int main(){
     int V[]={1, 2, 3}, i;
     TCircList* lista1 = list_create();
-    TCircList* lista2 = list_create();
     for(i=0; i<3; i++){
-        if(!list_insert_begin(lista1, V[i]))
+        if(!list_insert_end(lista1, V[i]))
             printf("Nao consegui inserir na lista1 o valor V[%d] = %d\n", i, V[i]) ;   
-        if(!list_insert_end(lista2, V[i]))
-            printf("Nao consegui inserir na lista2 o valor V[%d] = %d\n", i, V[i]) ;   
+        list_print(lista1);
     }
-    TCircList* lista3 = list_concatenate(lista1, lista2);
-    list_print(lista3);
-    unsigned int n=4;
-    printf("Deletando %u primeiros elementos\n", n);
-    //list_delete_begin(lista3);    
-    if(!list_delete_n(lista3, n))
-        printf("Nao foi possível excluir %u elementos!\n", n);
-    list_print(lista3);
+    list_print(lista1);
     return 0;
 }
