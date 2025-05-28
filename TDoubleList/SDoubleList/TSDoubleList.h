7fd4d3fb2aab73862ba45f860e703074d29f9a9a
pprafaellima <pp.rafaellima@gmail.com>
2025-05-28 09:08:18 -0300

    Comentario da funçao insert

--- TDoubleList/SDoubleList/TSDoubleList.h
+++ TDoubleList/SDoubleList/TSDoubleList.h
@@ -10,21 +10,14 @@ typedef struct _list TSDoubleList;
  * */
 TSDoubleList* list_create();
 /**
- * Tenta fazer a inserção no início da lista. 
- * Por exemplo, se a sequência de inserção for 1, 2, 3, 
- * a lista será [3] -> [2] -> [1]
+ * Tenta fazer a inserção ordenada na lista
  * @param TSDoubleList* ponteiro para uma instância da lista ordenada
  * @param int informação a ser armazenada na lista de forma ordenada
  * @return bool informando o sucesso da operação
  */
 bool list_insert(TSDoubleList*, int);
 /**
- * Tenta fazer a inserção no fim da lista. 
- * Por exemplo, se a sequência de inserção for 1, 2, 3, 
- * a lista será [1] -> [2] -> [3]
- * @param TSDoubleList* ponteiro para uma instância da lista
- * @param int informação a ser armazenada na lista
- * @return bool informando o sucesso da operação
+ * Imprime do inicio para o fim.
  */
 void list_print(TSDoubleList*);
 /**
